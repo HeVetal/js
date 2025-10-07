b67ef46a326c633f20ca816a5b234ca448eadc5f
vetal <heavyve@gmail.com>
2025-10-07 20:52:22 +0300

    about functions

--- 03-theory-num-str-date./03-functions.js
+++ 03-theory-num-str-date./03-functions.js
@@ -1,11 +1,47 @@
 // Function Declaration
-function greet(name) {
-    console.log('Hello - ', name)
-}
-
-//Function Expression
-const greet2 = function (name) {
-    console.log('2 Hello - ', name)
-}
+//function greet(name) {
+//    console.log('Hello - ', name)
+//}
+//
+////Function Expression
+//const greet2 = function (name) {
+//    console.log('2 Hello - ', name)
+//}
 
 // console.dir() // about object
+//setTimeout(function(){
+////    console.log('Timeout')
+//    greet('vasa')
+//}, 1500)
+
+//let counter = 0
+//const interval = setInterval(function(){
+//    if (counter === 5) {
+//        clearInterval(interval)
+//    } else {
+//        console.log(++counter)
+//    }
+//}, 1000)
+
+//function greet(name) {
+//    console.log('Hello - ', name)
+//}
+//const arrow = (name) => {
+//    console.log('Hello - ', name)
+//}
+//
+//arrow('ve')
+
+//function pow(num, exp){
+//    return Math.pow(num, exp)
+//}
+//
+//function pow2 = (num, exp) => Math.pow(num, exp)
+
+const sum = (a = 40, b = a /2) => a + b
+
+function sumAll(... numbers){
+    let res = 0
+
+    return res
+}
