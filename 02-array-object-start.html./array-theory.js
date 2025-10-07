43985e571df12e9cab06ecb8bd7c222d63b804c9
vetal <heavyve@gmail.com>
2025-10-07 16:12:53 +0300

    add string's methods

---
+++ 02-array-object-start.html./array-theory.js
@@ -0,0 +1,51 @@
+const names = ['Vlad', 'Helen', 'Igor', 'Ksu'];
+
+// names.push('Alena') // add back to array
+// names.unshift('January'); // add begin to array
+// names.shift() // delete first element, return it
+// names.pop() // delete last element, return it
+// names.reverse() // reverse array
+// names.toReversed() // return new reverse array
+// names.sort() // sort array
+// names.toSorted() // return new sort array
+// names.splice(2,1) // delete 1 element from index 2
+// names.toSpliced(2,1) // return new array, with 1 element from index 2
+// names.indexOf(element)// return index element, if not return -1
+// names.with(index, '')// change index to value''
+// names.map() // mapping array
+
+// const capitalNames = names.map(function (name) {
+//     // const newName = name + '!'
+//     // return newName
+//     return name.toUpperCase()
+// })
+
+// names.includes() // find element true or false.The same method, names.indexOf('') !== -1
+
+//Object
+const people = [
+    {name: 'Vlad', budget: 4200},
+    {name: 'Helen', budget: 15100},
+    {name: 'Igor', budget: 300},
+    {name: 'Ksu', budget: 7520}
+]
+
+// people.find(function (person) {}) // find object to array
+
+// const findedPerson =
+    // people.find(function (person) {
+    // return person.budget === 7520})
+    //the same
+    // people.find((p) => p.budget === 7520);
+// people.findIndex() // return object index
+// people.filter() // return new filtered array objects
+
+// const string = 'Hi, you?'
+//
+// console.log(string.split('')
+//     .toReversed()
+//     .join('!')
+//     .split('')
+//     .filter(x => x !== '!').join(''))
+
+
