a79aae13b8639b2bb8b27f60397de1e636c3dbd4
vetal <heavyve@gmail.com>
2025-10-07 16:14:36 +0300

    about strings

---
+++ 03-theory-num-str-date./02-stings.js
@@ -0,0 +1,14 @@
+// const string = '' // classic
+// const string2 = `` // new, plus ${} and /n
+
+// const name = 'name'
+// name.length //
+// name.toUpperCase() // all big
+// name.toLowerCase() // all small
+// name.charAt(2) // return simbol to index 'm'
+// name.indexOf('a') // return index to simbol '1'
+// name.startsWith('na') // return boolean
+// name.endWith('na') // return boolean
+// name.repeat(2) // repeat 2 times
+// name.trim()// delete spase
+// name.padStart(10) // plus 10 spase
