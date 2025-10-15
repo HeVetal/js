27435ec7f91b80e21d496f7bfdfdb906172b355b
vetal <heavyve@gmail.com>
2025-10-15 22:08:27 +0300

    example global variables

---
+++ nodeJS/Events/app.js
@@ -0,0 +1,28 @@
+const EventEmmiter = require('events')
+
+const myEmitter = new EventEmmiter()
+
+const logDbConnection = () => {
+    console.log('Connected to DB')
+}
+
+// myEmitter.addListener('connected', logDbConnection)
+myEmitter.on('connected', logDbConnection)
+myEmitter.emit('connected')
+
+// myEmitter.removeListener('connected', logDbConnection)
+// myEmitter.off('connected', logDbConnection)
+// myEmitter.removeAllListeners('connected')
+// myEmitter.emit('connected')
+
+// myEmitter.on('msg', (data) => {
+//     console.log(`Got: ${data}`)
+// });
+//
+// myEmitter.emit('msg', 'Hello! Get my message')
+
+myEmitter.once('off', () => {
+    console.log('I got one times and delete')
+})
+
+myEmitter.emit('off')
