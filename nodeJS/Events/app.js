2c3070fd7b4381fdf87df2248b4247e113590cec
vetal <heavyve@gmail.com>
2025-10-19 13:21:49 +0300

    add, delete, count listeners

--- nodeJS/Events/app.js
+++ nodeJS/Events/app.js
@@ -7,23 +7,49 @@ const logDbConnection = () => {
 }
 
 // myEmitter.addListener('connected', logDbConnection)
-myEmitter.on('connected', logDbConnection)
-myEmitter.emit('connected')
+// myEmitter.on('connected', logDbConnection)
+// myEmitter.emit('connected')
 
 // myEmitter.removeListener('connected', logDbConnection)
 // myEmitter.off('connected', logDbConnection)
 // myEmitter.removeAllListeners('connected')
 // myEmitter.emit('connected')
 
-// myEmitter.on('msg', (data) => {
-//     console.log(`Got: ${data}`)
-// });
-//
-// myEmitter.emit('msg', 'Hello! Get my message')
+myEmitter.on('msg', (data) => {
+    console.log(`Got: ${data}`)
+});
 
-myEmitter.once('off', () => {
-    console.log('I got one times and delete')
+myEmitter.emit('msg', 'Hello! Get my message')
+
+// myEmitter.once('off', () => {
+//     console.log('I got one times and delete')
+// })
+//
+// myEmitter.emit('off')
+// myEmitter.emit('off')
+
+// console.log(myEmitter.getMaxListeners())
+// myEmitter.setMaxListeners(1)
+// console.log(myEmitter.getMaxListeners())
+// console.log(myEmitter.listenerCount('msg')) // get count listener this event
+// myEmitter.prependListener() //add listener to the beginning of the list
+// myEmitter.prependOneListener()
+//
+// console.log(myEmitter.listeners('msg')) // get name listener
+console.log(myEmitter.eventNames())
+
+// Errors
+myEmitter.on('error', (err) => {
+    console.log(`Get Error: ${err.message}`)
 })
 
-myEmitter.emit('off')
-myEmitter.emit('off')
+myEmitter.emit('error', new Error('Boom'))
+
+const target = new EventEmmiter()
+
+const logTarget = () => {
+    console.log('connected to target')
+}
+
+target.addEventListener('connected', logTarget)
+target.dispatchEvent(new Event('connected'))
